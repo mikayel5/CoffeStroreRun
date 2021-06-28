fb9ed43fad9c7303bbbeb4f732b6d0f40da863af
Your Name <mikayelnahapetyan@gmail.com>
2021-06-28 21:26:13 +0400

    Add orders

--- scripts/datastore.js
+++ scripts/datastore.js
@@ -6,22 +6,22 @@ function DataStore(){
     this.data = {}
 
    
-}
+};
 DataStore.prototype.add = function (key, val){
     this.data[key]= val;
-}
+};
 
 DataStore.prototype.get = function (key){
     return this.data[key];
-}
+};
 
-DataStore.prototype.gatAll = function(){
+DataStore.prototype.gatAll = function (){
     return this.data;
-}
+};
 
 DataStore.prototype.remove = function(key){
     delete this.data[key];
-}
+};
 
 App.DataStore = DataStore;
 window.App = App;
